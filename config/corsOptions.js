08f2a8c2bafd05b3263d3acd07a508db5017ba99
jylida <106566574+jylida@users.noreply.github.com>
2022-10-20 01:22:38 +0800

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,12 +1,6 @@
 const allowedOrigin = require("./allowedOrigin");
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
   optionsSuccessStatus: 200,
