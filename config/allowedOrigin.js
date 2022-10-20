4cfe806bda564719c2cb32d543527950078c161c
zhenqu <zhenqu.zhang@gmail.com>
2022-10-20 18:52:50 +0800

    添加http的冒号

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,8 @@
 const allowedOrigin = [
   "http://localhost:3500/api1",
   // "https://www.google.com",
-  "https//jingyeschool.org.cn",
-  "https//jingyeschool.org.cn:8443",
+  "https://jingyeschool.org.cn",
+  "https://jingyeschool.org.cn:8443",
   "http://localhost:3000",
   "https//jingyeschool.org.cn:3500",
 ];
