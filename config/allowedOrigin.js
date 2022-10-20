e0438684253638d3d85de76f8d17a0d4afea5e2b
DA LI <li_da_jingye@163.com>
2022-10-20 16:59:11 +0800

    add adresses to allowedOrigin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,8 @@
 const allowedOrigin = [
   "http://localhost:3500/api1",
-  "https://www.google.com",
+  // "https://www.google.com",
+  "https//www.jingyeschool.org.cn",
+  "https//www.jingyeschool.org.cn:8443",
   "http://localhost:3000",
 ];
 
