77a6e4c5d221146d13e28e676de9507d40da1c73
zhenqu <zhenqu.zhang@gmail.com>
2022-10-20 21:56:06 +0800

    修改allowedOrigin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,10 +1,8 @@
 const allowedOrigin = [
-  "http://localhost:3500/api1",
-  // "https://www.google.com",
+  "http://localhost:3500",
+  "http://localhost:3000",
   "https://jingyeschool.org.cn",
   "https://jingyeschool.org.cn:8443",
-  "http://localhost:3000",
-  "https//jingyeschool.org.cn:3500",
 ];
 
 module.exports = allowedOrigin;
